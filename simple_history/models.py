607a3f7805fe459476fe9b91587cacadbec816df
bixtape <jotasos@msn.com>
2015-06-24 14:04:31 -0300

    added unicode to verbose name

--- simple_history/models.py
+++ simple_history/models.py
@@ -175,7 +175,7 @@ class HistoricalRecords(object):
         }
 
         if model._meta.verbose_name:
-            meta.update({'verbose_name': 'Historical ' + model._meta.verbose_name})
+            meta.update({'verbose_name': 'Historical ' + unicode(model._meta.verbose_name)})
 
         return meta
 
